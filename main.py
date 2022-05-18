3f1e23fa21f8e6634ba44114c4fc24f6a6a90841
Rgelal13@gmail.com <rgelal13@gmail.com>
2022-05-18 15:04:52 +0545

    ppo

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-from stable_baselines3 import A2C, DQN
+from stable_baselines3 import A2C, DQN, PPO
 from stable_baselines3.common.callbacks import CheckpointCallback
 from stable_baselines3.common.env_checker import check_env
 from CustomEnv import CustomEnv, deck
 from stable_baselines3.common.env_util import make_vec_env
+vecEnv = CustomEnv()
 vecEnv = make_vec_env(CustomEnv, 11)
-env = CustomEnv()
 # It will check your custom environment and output additional warnings if needed
 # check_env(env)
 checkpoint_callback = CheckpointCallback(save_freq=1000000, save_path='./logs/',
                                          name_prefix='rl_model')
 
-model = DQN('MlpPolicy', vecEnv, verbose=0)
+model = PPO('MlpPolicy', vecEnv, verbose=0)
+# model.learn(total_timesteps=100)
 model.learn(total_timesteps=13375000, callback=checkpoint_callback)
-# model.learn(total_timesteps=10000)
 # model = DQN.load("model (1).zip")
 model.save("model")
-obs = env.reset()
-for i in range(5):
+obs = vecEnv.reset()
+for i in range(13):
     action, _state = model.predict(obs, deterministic=True)
-    obs, reward, done, info = env.step(action)
+    obs, reward, done, info = vecEnv.step(action)
     print(info)
     print(reward)
     print("")
@@ -26,6 +26,6 @@ for i in range(5):
     print("")
     print("")
     print("")
-    env.render()
+    vecEnv.render()
     if done:
-        obs = env.reset()
+        obs = vecEnv.reset()
